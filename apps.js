fad3c7677fdf1477555037ba58513d5351aa5fe2
CharlesFahrenwald <75191202+CharlesFahrenwald@users.noreply.github.com>
2020-12-21 16:39:51 -0700

    Update apps.js

--- apps.js
+++ apps.js
@@ -9,125 +9,86 @@ function generateStartScreenHtml(){
     `;
 }
 
-
-
-
-function handleStartClick(){
-    $("#start").on('click', function (event) {
-        //alert("Clicked start")
-        generateQuestionHtml();
-});
-}
-
-
-
-
-function generateQuestionNumberAndScoreHtml(){
- const html = $(`<ul class="question-and-score">
+function displayQuestionsAndScore(){
+    return `
+ <ul class="question-and-score">
     <li id="question-number">
     Queston Number: ${store.questionNumber + 1}/${store.questions.length}
     </li>
     <li id = "score">
     Score: ${store.score}/${store.questions.length}
     </li>
-    </ul>`);
-    $(".question-and-score").html(html);
+    </ul>
+  `;
 }
-
-
-
-
-
-function generateAnswersHtml() {
+//render
+function displayAnswers() {
     //this function inputs our answers for each question
-  let question = store.questions[store.questionNumber];
- for(let i=0; i < question.options.length; i++)
- {
-    $('.js.answers').append(`
-        <input type = "radio" name="options" id="option${i + 1}" value = "${question.options[ i ]}" tabindex = "${i + 1}"> 
-        <label for ="option${i+1}"> ${question.options[i]}</label> <br/>
-        <span id="js-r${i + 1}"></span>
- `);     
-}
+    const allAnswers = store.questions[store.questionNumber].answers
+    let answersHtml = '';
+    let i = 0;
+    allAnswers.forEach(answer => {
+        answersHtml += `
+        <div id="option-container-${i}">
+        <input type = "radio" name="option" id="option${i + 1}" value= "${answer}" tabindex= "${i+1}" required>
+        <label for "option${i + 1}"> ${answer}</label>
+        </div>`;
+        i++;
+    });
+    return answersHtml;
 }
 
-        
-        
-        
-        
-function generateQuestionHtml() {
+function displayQuestions() {
     // this function inputs our questions into our html 
-    let question = store.questions[store.currentQuestion];
-    generateQuestionAndScoreHtml();
-        const questionHtml = $(` 
-     <div>
-       <form id= "js-question" class="question-form"
-        
-           <fieldset>
-               <div class="row question">
-                <legend> ${question.question}</legend>
-                </div>
-                               
-               <div class= "row options">
-               <div class="js-options"> </div>
-               </div>               
-  <div class= "row">
-          <button type = "submit" id="answer" tabindex="5">Submit</button>
-          <button type = "button" id="next-question" tabindex="6"> Next >></button>
-      </div>
-    </fieldset>
+    let questionNumber = store.questions[store.questionNumber];
+    return `
+    <form id="question-form" class="queston-form">
+        <fieldset>
+            <div class="question">
+            <legend>${questionNumber.question}</legend>
+            </div>
+        <div class="options">
+             <div class="answers">
+            ${displayAnswers()}
+             </div>
+            </div>
+            <button type="submit" id="submit-answer-btn" tabindex="5">Submit</button>
+            <button type="button" id="next-question-btn" tabindex="6"> Next &gt;</button>
+        </fieldset>
     </form>
-  </div>`);
-    $("main").html(questionHtml);
-    updateOptions();
-    $("#next-question").hide();                     
-}
-     
-     
-     
-     
-     
-function handleQuestionFormSubmission() {
-  $('body').on('submit', '#js-questions' , function (event){
-      event.preventDefault();
-      let current = store.questions[store.currentQuestion];
-      let selectedOption = $('input[name=options]:checked').val();
-      if (!selectedOption) {
-          
-            alert("Choose an option");
-            return;
-        }
-        let number = current.options.findIndex(i => i === selectedOption);
-        let id = "#js-r" + ++number;
-        $('span').removeClass("correct incorrect");
-        if(selectedOption === current.answer) {
-            store.score++;
-            $(`<img src='./icons8-offline-pin-100.png' alt='correct-icon'><br />You got it right!<br/>`).insertBefore("#next-question");
-            $('.question').addClass("hidden");
-            $('.options').addClass("hidden");
-            $(`fieldset`).addClass("correct");
-        }
-        else {
-            $(`<img src='./icons8-error-100.png' alt='incorrect-icon'><br />incorrect... <br/> The correct answer was "${current.answer}"<br/>`).insertBefore("#next-question");
-            $("fieldset").addClass("incorrect");
-            $('.question').addClass("hidden");
-            $('.options').addClass("hidden");
-        }
+        `;
 
-        store.currentQuestion++;
-        $("#js-score").text(`Score: ${store.score}/${store.questions.length}`);
-        $('#answer').hide();
-        $("input[type=radio]").attr('disabled', true);
-        $('#next-question').show();
+
+
+}
+function handleQuestionFormSubmission() {
+  $('body').on('submit', '#question-form' , function (event){
+      event.preventDefault();
+      const questionNumber = store.questions[store.questionNumber];
+      let selectedOption = $('input[name=option]:checked').val();
+      let optionContainerID = `#option-container-${questionNumber.answers.findIndex(i => i === selectedOption)}`;
+      if (selectedOption === questionNumber.correctAnswer) {
+        store.score++;
+        console.log("test", store.score)
+        $(optionContainerID).append(generateFeedbackHTML('correct'));
+      }
+      else { 
+        $(optionContainerID).append(generateFeedbackHTML('incorrect'));
+    }
+    store.questionNumber++;
+    $('#submit-answer-btn').hide();
+    $('input[type=radio]').each(() => {
+        $('input[type=radio]').attr('disable', true);
+    });
+    $('#next-question-btn').show();
   });
   }
-     
-     
-     
-     
-     
-function generateResultsScreen(){
-let resultHtml = $( `
+
+
+
+
+function resultsScreen(){
+return `
 <div class="results">
   <form id="js-restart-quiz">
     <fieldset>
@@ -144,38 +105,73 @@ let resultHtml = $( `
     </fieldset>
 </form>
 </div>
-`);
-    store.currentQuestion = 0;
-    store.score = 0;
-    $("main").html(resultHtml);
+`;
 }
 
-     
-     
-     
- function endOfQuiz() {
-     $('body').on('click', '#next-question',(event) => {
-         store.currentQuestion === store.questions.length
-         ? generateResultsScreen()
-         : generateQuestionHtml();
-     });
+function generateFeedbackHTML(answerStatus) {
+    let correctAnswer = store.questions[store.questionNumber].correctAnswer;
+    let htmlToDisplay = '';
+    if (answerStatus === 'correct') {
+        htmlToDisplay = `<div class="right-answer">Correct</div>`;
+    }
+    else if (answerStatus === 'incorrect') {
+        htmlToDisplay =`<div class="wrong-answer">Incorrect. The correct answer is ${correctAnswer}.</div>`;
+    }
+    return htmlToDisplay; 
  }
 
-     
-     
-     
-     
+
+
+function handleStartButton(){
+    $("main").on('click', '#start-btn', function (event) {
+        //alert("Clicked start")
+        store.quizStarted = true;
+        renderQuizHTML();
+});
+}
+
+function handleNextQuestionClick(){  
+    $('body').on('click', '#next-question-btn', (event) => {
+    renderQuizHTML();
+  });
+}
+
+function restartQuiz(){
+    store.quizStarted = false;
+    store.questionNumber = 0;
+    store.score = 0;
+}
+
 function handleRestartButtonClick() {
-    $('body').on('click','#restart',(event)=> {
-        generateQuestionHtml();
+    $('body').on('click','#restart',()=> { 
+        restartQuiz();
+        renderQuizHTML();
     });
 }
 
 
 
-function handleQuizApp(){
-    handleStartClick();
-    endOfQuiz();
+function renderQuizHTML(){
+    let htmlToDisplay = '';
+    if(store.quizStarted === false){  
+        htmlToDisplay = generateStartScreenHtml();
+        $("main").html(htmlToDisplay);
+    }
+    else if(store.questionNumber >= 0 && store.questionNumber < store.questions.length){
+        htmlToDisplay = displayQuestionsAndScore();
+        htmlToDisplay += displayQuestions();
+        $("main").html(htmlToDisplay);
+    }
+else {
+    $('main').html(resultsScreen());
+}
+
+}
+
+function handleQuizApp() {
+    renderQuizHTML();
+    handleStartButton();
+    handleNextQuestionClick();
     handleQuestionFormSubmission();
     handleRestartButtonClick();
 }
